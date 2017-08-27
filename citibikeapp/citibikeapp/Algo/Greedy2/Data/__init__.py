0e0e3d547ca2bc5fbb9934033ac1fcd31e8c17a3
yushangdi <sy@shangdis-mbp.home>
2017-08-26 22:35:53 -0400

    Remove Greedy2

--- citibikeapp/citibikeapp/Algo/Greedy2/Data/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: sy
-# @Date:   2017-06-21 12:55:37
-# @Last Modified by:   sy
-# @Last Modified time: 2017-06-21 12:55:37
--- citibikeapp/citibikeapp/Algo/Greedy2/create_StationMap.py
+++
@@ -1,7 +0,0 @@
-import cPickle as pickle
-        #from Algo.Greedy2 import Main, StationMap
-from StationMap import Task, Station, Truck, StationMap
-from Main import Routes
-x = StationMap()
-with open('/var/www/html/flaskapp/Algo/Greedy2/Data/StationMap.txt', 'w') as output:
-    pickle.dump(x, output, pickle.HIGHEST_PROTOCOL)
